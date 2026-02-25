91d8853603daf4a20dec551a2ab93b0f17bf400b
Nikola Dobrosavljevic <nikola.dobrosavljevic@mera.com>
2026-02-25 15:10:11 +0100

    Finished init

--- Frontend/src/models/Appointment.model.ts
+++ Frontend/src/models/Appointment.model.ts
@@ -1,20 +1,20 @@
 export interface Appointment {
-  id: string;                // GUID
-  doctor: string;            // "Ime Prezime"
-  patient: string;           // "Ime Prezime"
-  type: string;              // "Konsultacija" / "Kontrola" / "Hitno"
-  status: AppointmentStatus; // "Scheduled" | "Completed" | "Cancelled"
+  id: string;                
+  doctor: string;            
+  patient: string;           
+  type: string;              
+  status: AppointmentStatus; 
   start: Date;
   end: Date;
   notes: string;
 }
 
 export interface CreateAppointmentDto {
-  doctorId: string;   // GUID
-  patientId: string;  // GUID
-  type: number;       // 0/1/2 (backend enum)
-  startTime: string;  // ISO string
-  endTime: string;    // ISO string
+  doctorId: string; 
+  patientId: string; 
+  type: number;       
+  startTime: string;  
+  endTime: string;    
   notes: string;
 }
 
