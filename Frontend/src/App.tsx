eda7ce2647b745be6ee301b18aea39412814ab44
Nikola Dobrosavljevic <nikola.dobrosavljevic@mera.com>
2026-02-26 20:44:37 +0100

    Create + Read Doctor

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import AppointmentsPage from "./components/AppointmentPage";
 import PatientsPage from "./components/PatientsPage";
+import DoctorsPage from "./components/DoctorsPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/appointments" element={<AppointmentsPage />} />
       <Route path="/patients" element={<PatientsPage />} />
+      <Route path="/doctors" element={<DoctorsPage />} />
       <Route path="*" element={<Navigate to="/appointments" replace />} />
     </Routes>
   );
