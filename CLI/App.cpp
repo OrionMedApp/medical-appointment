b5db55c0a71a235e9278859adcdb6fdaa84a4229
Petar Trifuljesko <petar.trifuljesko@mera.com>
2026-02-26 16:00:11 +0100

    Added Track new appointments

--- CLI/App.cpp
+++ CLI/App.cpp
@@ -106,9 +106,19 @@ void App::stateMachine() {
                 break;
             case STORE_ENTRIES:
             case TRACK_APPOINTMENTS:
-            case SYNC_DOCTORS:
-                hospitalManager->fetchFileFromBackend(L"CLI-DoctorApp",L"localhost", 5085, L"/api/Doctor/export", "doctors");
+                hospitalManager->trackAppointments();
+                FlushConsoleInputBuffer(GetStdHandle(STD_INPUT_HANDLE));
                 break;
+            case SYNC_DOCTORS: {
+                DWORD statusCode = 0;
+                std::string response = hospitalManager->getResponseFromBackend(L"CLI-DoctorApp",L"localhost", 5085, L"/api/Doctor/export", statusCode);
+                if (statusCode == 200) {
+                    hospitalManager->exportResponseToAFile(response, "doctors");
+                }else {
+                    cout << "Error occured!" << endl << endl;
+                }
+                break;
+            }
             case EXIT:
                 isRunning = false;
                 return;
--- CLI/HospitalManager.cpp
+++ CLI/HospitalManager.cpp
@@ -14,6 +14,8 @@
 #include <iomanip>
 #include <sstream>
 #include <chrono>
+#include <set>
+#include <thread>
 
 #include "HttpClient.hpp"
 
@@ -22,14 +24,13 @@
 using json = nlohmann::json;
 
 
-
-void HospitalManager::addSaveDoctor(Doctor& d) {
+void HospitalManager::addSaveDoctor(Doctor &d) {
     std::ifstream inFile(doctors_file);
     json doctors;
     if (inFile.good()) {
         inFile >> doctors;
         inFile.close();
-    }else {
+    } else {
         doctors = json::array();
     }
 
@@ -39,7 +40,8 @@ void HospitalManager::addSaveDoctor(Doctor& d) {
     outFile << doctors.dump(4);
     outFile.close();
 }
-void HospitalManager::addSavePatient(Patient& p) {
+
+void HospitalManager::addSavePatient(Patient &p) {
     std::ifstream inFile(patients_file);
     json patients;
     if (inFile.good()) {
@@ -55,7 +57,8 @@ void HospitalManager::addSavePatient(Patient& p) {
     outFile << patients.dump(4);
     outFile.close();
 }
-void exportResponseToAFile(std::string& response, std::string file_name) {
+
+void HospitalManager::exportResponseToAFile(std::string &response, std::string file_name) {
     std::cout << "CSV fetched - " << response.length() << " bytes!" << std::endl;
 
     // CSV filename: YYYYMMDD_HHMMSS_doctors.csv
@@ -71,20 +74,67 @@ void exportResponseToAFile(std::string& response, std::string file_name) {
     std::cout << "File saved" << std::endl << std::endl;
 }
 
-bool HospitalManager::fetchFileFromBackend(const std::wstring& appName, const std::wstring& host, const int& port, const std::wstring& path, std::string file_name) {
-    auto& client = HttpClient::getInstance();
+std::string HospitalManager::getResponseFromBackend(const std::wstring &appName, const std::wstring &host,
+                                                    const int &port, const std::wstring &path, DWORD &statusCode) {
+    auto &client = HttpClient::getInstance();
     client.connect(appName, host, port);
-    DWORD statusCode = 0;
+    statusCode = 0;
     std::string response = client.getRequest(path, statusCode);
     client.disconnect();
 
 
-    if (statusCode == 200) {
-        exportResponseToAFile(response, file_name);
-        return true;
-    }
+    return response;
+}
 
-    return false;
+void HospitalManager::trackAppointments() {
+    DWORD statusCode = 0;
+
+    // Create the filename ONCE when the command starts
+    auto now = std::chrono::system_clock::now();
+    auto time_t = std::chrono::system_clock::to_time_t(now);
+    std::stringstream ss;
+    ss << "Report_" << std::put_time(std::localtime(&time_t), "%Y%m%d_%H%M%S") << ".txt";
+    std::string reportFile = ss.str();
+
+    std::set<std::string> seenLines;
+    std::cout << "Monitoring... Saving to " << reportFile << std::endl;
+    int cnt = 1;
+    bool running = true;
+    while (running) {
+        std::string response = getResponseFromBackend(L"CLI-DoctorApp", L"localhost", 5085, L"/api/Appointment/export", statusCode);
+
+        if (statusCode == 200) {
+            std::stringstream responseStream(response);
+            std::string line;
+
+            std::getline(responseStream, line); // Skip header
+            while (std::getline(responseStream, line)) {
+                if (line.empty()) continue;
+
+                if (seenLines.find(line) == seenLines.end()) {
+                    seenLines.insert(line);
+
+                    std::cout << "\n--- New Appointment "  << cnt << "---\n" << line << std::endl;
+                    cnt++;
+                    std::ofstream outFile(reportFile, std::ios::app);
+                    outFile << line << std::endl;
+                }
+            }
+        } else {
+            std::cout << "API Error! " << statusCode << std::endl;
+        }
+
+        std::cout << "Press ESC to exit... " << std::endl << std::endl;
+
+        for (int i = 0; i < 250; i++) {
+            if (GetAsyncKeyState(VK_ESCAPE) & 0x8000) {
+                running = false; // Break out of the sleep loop if a key is pressed
+                break;
+            }
+
+            std::this_thread::sleep_for(std::chrono::milliseconds(20));
+        }
+    }
 }
 
 
